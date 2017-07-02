c5ce04763710903ed122f21224d0eba7d720f8d9
HHFV <henry.dos@gmail.com>
2017-07-02 11:40:22 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<meta charset="UTF-8"> 
+<title></title> 
+</head> 
+<body> 
+<form name="Formulario" method="get" action=""> 
+Numero1: <input type="text" name="N1"> 
+<br><br> 
+Numero2: <input type="text" name="N2"> 
+<br><br> 
+<input type="submit" name="Sumar" value="Sumar" 
+</form> 
+</body> 
+</html> 
